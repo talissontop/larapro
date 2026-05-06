9f39bf311d56e49925f2eeefb56855dc6a80c8af
talissontop <72979419+talissontop@users.noreply.github.com>
2026-05-06 16:00:05 -0300

    Modify PWA metadata and icon sources
    Updated the description and start URL for the PWA. Changed icon sources to use external URLs.

--- sw.js
+++ sw.js
@@ -2,26 +2,22 @@
   "id": "/larapro/",
   "name": "Lara Elite v31.0 OMEGA",
   "short_name": "Lara Elite",
-  "description": "Interface Lara Elite PWA — experiência imersiva em modo aplicativo.",
-  "start_url": "/larapro/",
+  "description": "Interface Neural de Alta Performance",
+  "start_url": "/larapro/index.html",
   "scope": "/larapro/",
   "display": "standalone",
-  "display_override": [
-    "standalone",
-    "minimal-ui"
-  ],
   "orientation": "portrait",
   "background_color": "#050811",
   "theme_color": "#050811",
   "icons": [
     {
-      "src": "/larapro/icons/192.png",
+      "src": "https://spicy-harlequin-pqaujlkovi.edgeone.app/pngtree-beautiful-ai-generated-girl-so-much-attractive-png-image_12342109.png",
       "sizes": "192x192",
       "type": "image/png",
       "purpose": "any maskable"
     },
     {
-      "src": "/larapro/icons/512.png",
+      "src": "https://spicy-harlequin-pqaujlkovi.edgeone.app/pngtree-beautiful-ai-generated-girl-so-much-attractive-png-image_12342109.png",
       "sizes": "512x512",
       "type": "image/png",
       "purpose": "any maskable"
